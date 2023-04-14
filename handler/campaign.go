6428309395915eb7eece284f7611cae2be868eb4
Mujahid <mujahid.170997@gmail.com>
2023-04-14 22:06:50 +0700

    get campaigns endpoint (WIP)

--- handler/campaign.go
+++ handler/campaign.go
@@ -1,5 +1,36 @@
 package handler
 
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/mujahxd/crowdfunding/campaign"
+	"github.com/mujahxd/crowdfunding/helper"
+)
+
 // tankap param
 // handler maping ke service
 // service tentukan repo yang dipakai
+
+type campaignHandler struct {
+	service campaign.Service
+}
+
+func NewCampaignHandler(service campaign.Service) *campaignHandler {
+	return &campaignHandler{service}
+}
+
+// api/v1/campaigns
+func (h *campaignHandler) GetCampaigns(c *gin.Context) {
+	userID, _ := strconv.Atoi(c.Query("user_id"))
+
+	campaigns, err := h.service.GetCampaigns(userID)
+	if err != nil {
+		response := helper.APIResponse("error to get campaigns", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+	response := helper.APIResponse("List of campaigns", http.StatusOK, "sucess", campaigns)
+	c.JSON(http.StatusOK, response)
+}
--- main.go
+++ main.go
@@ -33,6 +33,7 @@ func main() {
 	authService := auth.NewService()
 
 	userHandler := handler.NewUserHandler(userService, authService)
+	campaignHandler := handler.NewCampaignHandler(campaignService)
 
 	router := gin.Default()
 	api := router.Group("/api/v1")
@@ -41,6 +42,8 @@ func main() {
 	api.POST("/sessions", userHandler.Login)
 	api.POST("/email_checkers", userHandler.CheckEmailAvailability)
 	api.POST("/avatars", authMiddleware(authService, userService), userHandler.UploadAvatar)
+
+	api.GET("/campaigns", campaignHandler.GetCampaigns)
 
 	router.Run()
 
